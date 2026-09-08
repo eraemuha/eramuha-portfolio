202122efcf30e00a3afd91dcb7e45e3e1f7eeea0
Era Muha <eraemuha@eramuha.com>
2026-09-08 21:34:52 +0200

    created new project: filemanager. Updated pyart

---
+++ projects/filemanager/filemanager.py
@@ -0,0 +1,11 @@
+# import statement
+# os for interacting with the operating system
+import os
+
+# assignment statement
+# listdir() returns a list of all files and directories in the specified path
+directory_contents = os.listdir(".")
+
+# method call
+# show directory contents
+print(directory_contents)
--- projects/pyart/pyart.py
+++ projects/pyart/pyart.py
@@ -7,9 +7,9 @@ import random
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 pyart = turtle.Turtle()
-pyart.width(20)
+pyart.width(10)
 
-# Compund statement
+# Compound statement
 for step in range(100):
     # Change this to use a random number.
     angle = random.randint(-90,90)
